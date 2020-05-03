3f498782ea0d58074e86b93d8f91c078f387eb08
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-05-03 20:50:05 +0200

    Updates

---
+++ precache-manifest.6b899a0d12d4a6bb9382f9385624bb95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9772adf9030fd9e541540c1dce5f4bf",
+    "url": "/covid-2019-app/index.html"
+  },
+  {
+    "revision": "7cd19895836b839bcaf2",
+    "url": "/covid-2019-app/static/css/2.0ba93a2d.chunk.css"
+  },
+  {
+    "revision": "e61ea2f28cbd2277a830",
+    "url": "/covid-2019-app/static/css/main.357f336c.chunk.css"
+  },
+  {
+    "revision": "7cd19895836b839bcaf2",
+    "url": "/covid-2019-app/static/js/2.b2a0b8ba.chunk.js"
+  },
+  {
+    "revision": "6d94984a5fdbad217098935a3d45084e",
+    "url": "/covid-2019-app/static/js/2.b2a0b8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61ea2f28cbd2277a830",
+    "url": "/covid-2019-app/static/js/main.b107d573.chunk.js"
+  },
+  {
+    "revision": "469843194c611af7f52b",
+    "url": "/covid-2019-app/static/js/runtime-main.1ccae784.js"
+  }
+]);
